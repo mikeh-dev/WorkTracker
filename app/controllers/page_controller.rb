c34ff420e0f4871fa586744af89a4a792231f951
mikeh112@hotmail.com <mikeh112@hotmail.com>
2024-11-05 11:27:14 +0000

    Added main controller for dashboard and some layouts from Raisui

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -1,2 +1,30 @@
 class PageController < ApplicationController
+
+	def help_center
+		render layout: 'admin'
+	end
+
+	def edit_booking
+		render layout: 'admin'
+	end
+
+	def booking
+		render layout: 'admin'
+	end
+
+	def new_booking
+		render layout: 'admin'
+	end
+
+	def bookings
+		render layout: 'admin'
+	end
+
+	def calendar
+		render layout: 'admin'
+	end
+
+	def dashboard
+		render layout: 'admin'
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,20 @@
 Rails.application.routes.draw do
+	get 'admin/help_center', to: 'page#help_center'
+	get 'admin/edit_booking', to: 'page#edit_booking'
+	get 'admin/booking', to: 'page#booking'
+	get 'admin/new_booking', to: 'page#new_booking'
+	get 'admin/bookings', to: 'page#bookings'
+	get 'admin/calendar', to: 'page#calendar'
+	get 'admin/dashboard', to: 'page#dashboard'
+  get 'main/dashboard'
   if Rails.env.development? || Rails.env.test?
     mount Railsui::Engine, at: "/railsui"
   end
 
-  # Inherits from Railsui::PageController#index
-  # To overide, add your own page#index view or change to a new root
-  # Visit the start page for Rails UI any time at /railsui/start
-  root action: :index, controller: "railsui/page"
+  root "main#dashboard"
 
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Defines the root path route ("/")
-  # root "posts#index"
 end
