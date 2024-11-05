dc59d1d7b3365ec475cec75e4e3e0abd2df15bc6
mikeh112@hotmail.com <mikeh112@hotmail.com>
2024-11-05 12:15:36 +0000

    Removed the test generators

--- spec/system/main_spec.rb
+++ spec/system/main_spec.rb
@@ -1,8 +1,16 @@
 require "rails_helper"
 
 RSpec.describe "Checks if a User is logged in before accessing the main dashboard", type: :system do
+  let(:user) { FactoryBot.create(:user) }
+
   it "redirects to the login page if the user is not logged in" do
     visit main_dashboard_path
     expect(page).to have_current_path(new_user_session_path)
+  end
+
+  it "allows a logged in user to access the main dashboard" do
+    login_as(user)
+    visit main_dashboard_path
+    expect(page).to have_current_path(main_dashboard_path)
   end
 end
