80e7bb939ce2178b5af9b8f89ebe21bcb3d599a5
mikeh112@hotmail.com <mikeh112@hotmail.com>
2024-11-05 12:18:41 +0000

    Switched to Postgres

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -37,8 +37,6 @@ end
 RSpec.configure do |config|
   config.include Devise::Test::IntegrationHelpers, type: :system
 
-  config.include FactoryBot::Syntax::Methods
-
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
